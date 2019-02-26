0bbc1f287f7bf98a7765ed99b871accedf20452d
Hnguyen18@worcester.edu <hnguyen18@worcester.edu>
2019-02-25 19:20:12 -0500

    stringTimes solved

---
+++ stringTimes.java
@@ -0,0 +1,23 @@
+/**
+February 25, 2019
+
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+stringTimes("Hi", 2) → "HiHi"
+stringTimes("Hi", 3) → "HiHiHi"
+stringTimes("Hi", 1) → "Hi"
+
+**/
+
+public String stringTimes(String str, int n) {
+  String result = "";
+  for(int i=0; i < n; i++){
+    result = result + str;
+  }
+  return result;
+}
+
+/**
+Correct answer: Yes
+**/
