1c56c56914b31194c6c55996f3eff7ef9fa47b73
Hnguyen18@worcester.edu <hnguyen18@worcester.edu>
2019-03-12 14:52:37 -0400

    another mod3

--- modThree.java
+++ modThree.java
@@ -1,13 +1,12 @@
 /**
-February 28, 2019
+March 7, 2019
+
+Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
 
 
-Given an array of ints, return true if the array contains either 3 even or 3 odd values all next to each other.
-
-
-modThree([2, 1, 3, 5]) → true
-modThree([2, 1, 2, 5]) → false
-modThree([2, 4, 2, 5]) → true
+count7(717) → 2
+count7(7) → 1
+count7(123) → 0
 
 **/
 
